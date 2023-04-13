d32cf8057587e6e2ec58d685bc56dad01b952f5a
martiarena842 <tomimartiarena@gmail.com>
2023-04-13 20:28:23 -0300

    ejercicio 3 hecho?

--- classes/clase6.sql
+++ classes/clase6.sql
@@ -1,3 +1,4 @@
+-- Active: 1680704245221@@127.0.0.1@3306@sakila
 use sakila;
 
 #List all the actors that share the last name. Show them in order
@@ -11,19 +12,30 @@ WHERE EXISTS (
 )
 ORDER BY a.last_name ;
 
-#EJERCICIO2
+#EJERCICIO2 Actors that dont participate in any film
+SELECT  a.first_name, a.last_name
+FROM actor as a
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM film_actor
+    WHERE a.actor_id = film_actor.actor_id
+);
 
 
-
-
-#EJERCICIO 3
-select CONCAT (c.first_name,' ',c.last_name)as nombre 
+#EJERCICIO 3 Find customers that rented only one film
+select CONCAT (c.first_name,' ',c.last_name)as nombre_completo
 from customer as c
 where not exists (
   select *
   from rental r
   where customer_id=r.customer_id)
 ;
+select c.first_name,c.last_name 
+from customer as c
+where EXISTS
+;
+
+
 #EJERCICIO4  
 SELECT concat(c.first_name, ' ', c.last_name) as full_name
 FROM customer c
